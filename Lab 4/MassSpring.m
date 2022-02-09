30a09b25282bc4fad1bc53890ec9b2e6e0c199d7
Brando <bgsimpson@wpi.edu>
2022-02-09 11:48:34 -0500

    Done lab time

--- Lab 4/MassSpring.m
+++ Lab 4/MassSpring.m
@@ -21,7 +21,33 @@ Y = (k1*((b*s)+k2))/((m1*m2*2^4) + b*s^3*(m1 + m2) + (k1*m2 + (m1 + m2)*k2)*s^2 
 
 sys = tf(numerator, denominator)
 
-t = 20:0.2:120;
-step(sys,t)
 
-% sys_tf
+% Simulation
+ts = 0.001;
+t = 0:ts:60;
+step(sys,t)
+my_function_values = step(sys,t);
+grid on;
+
+[max_overshoot,Index] = max(my_function_values);
+
+stepinfo(sys)
+
+% Rise Time
+% 10%
+index_10 = min(find(my_function_values >= 0.1));
+time_10 = index_10 * ts;
+% 90%
+index_90 = min(find(my_function_values >= 0.9));
+time_90 = index_90 * ts;
+
+Rise_Time = time_90 - time_10
+
+% Peak Time
+Peak_Time = Index * ts
+
+% Max Overshoot
+max_overshoot
+
+% Settling Time (5%)
+
